7210c0718ba09010f07de5430da90a8ffc1fee90
EBabayan <elena.babayan.1994@mail.ru>
2020-07-05 23:03:31 +0300

    Создание файла login_page.py. Перенос всех правил поиска в locators.py.Перезаписала файд requirements.txt.

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_REGISTER_FORM), "Register form is not presented"
+
