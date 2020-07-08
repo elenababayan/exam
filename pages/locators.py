abb76820f1555983454e9f1aec4a2cc333f93de6
EBabayan <elena.babayan.1994@mail.ru>
2020-07-08 12:53:09 +0300

    Добавили обработку алерта

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,7 @@ class LoginPageLocators(LoginPage):
     LOGIN_URL = (By.CSS_SELECTOR, '#login_link')
     LOGIN_FORM = (By.ID, "#login_form")
     LOGIN_REGISTER_FORM = (By.ID, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '#btn.btn-lg.btn-primary.btn-add-to-basket')
+    
